bcb0f6d65ebf7df3fd6b2c076841976d6c27a250
seawolf <125524687+seawolf2357@users.noreply.github.com>
2024-09-19 07:19:55 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import { SimliClient } from "simli-client";
 
-const simli_faceid = "9f3a3361-41b4-4157-87e6-9e6e4557ca7f";
+const simli_faceid = "74bf81fc-853f-41f6-aaa8-a8772d784327";
 const elevenlabs_voiceid = "1W00IGEmNmwmsDeYy7ag";
 
 const simliClient = new SimliClient();
